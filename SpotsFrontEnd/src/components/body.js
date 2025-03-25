cf5751efacfe306b012226dd9c54d3e08243c011
PierreSalves <pierre040797@live.com>
2025-03-24 21:12:08 -0300

    validandoPesquisa

--- SpotsFrontEnd/src/components/body.js
+++ SpotsFrontEnd/src/components/body.js
@@ -1,10 +1,9 @@
 import { formHandler } from '../handlers/formHandler.js';
 import { Container, Row, Col, Form, Button, Card } from 'react-bootstrap'
-import { ModalAddSpot } from './modalAddSpot.js';
-import { ModalViewSpot } from './modalViewSpot.js';
+import { ModalAddSpot } from './ModalAddSpot.js';
+import { ModalViewSpot } from './ModalViewSpot.js';
 
 import React, { useState, useEffect } from 'react';
-// import SpotCard from './spotCard.js';
 import Swal from 'sweetalert2'
 
 export const BodyForm = () => {
@@ -26,8 +25,8 @@ export const BodyForm = () => {
         setSelectedSpotId(null);
     }
 
-    async function listSpots() {
-        await formHandler('get', 'https://localhost:7181/api/Spots/listSpots', 'searchForm')
+    const listSpots = () => {
+        formHandler('get', 'https://localhost:7181/api/Spots/listSpots', 'searchForm')
             .then((response) => {
                 if (typeof response.status != null && response.status === 400) {
                     Swal.fire({
@@ -36,13 +35,13 @@ export const BodyForm = () => {
                         text: 'Algo deu errado!',
                     })
                 } else {
-                    setSpots(response.spots);
+                    setSpots(response.spots);               
                 }
             });
     }
 
     useEffect(() => {
-        listSpots();
+        listSpots();      
     }, []);
 
     return (
@@ -52,24 +51,35 @@ export const BodyForm = () => {
                     <Col>
                         <Form.Group controlId="searchInput">
                             <Form.Label>Pesquisa</Form.Label>
-                            <Form.Control type="text" placeholder="Digite o que deseja encontrar" name="filter" />
+                            <Form.Control id="filterInput" type="text" placeholder="Digite o que deseja encontrar" name="filter" />
                         </Form.Group>
                     </Col>
                     <Col>
                         <Form.Group><Form.Label>&nbsp;</Form.Label></Form.Group>
                         <Button variant="primary" type="button" onClick={async () => {
-                            await formHandler('get', 'https://localhost:7181/api/Spots/filterSpots', 'searchForm')
-                                .then((response) => {
-                                    if (typeof response.status != null && response.status === 400) {
-                                        Swal.fire({
-                                            icon: 'error',
-                                            title: 'Oops...',
-                                            text: 'Algo deu errado!',
-                                        })
-                                    } else {
-                                        setSpots(response.spots);
-                                    }
-                                });
+                            if (document.getElementById('filterInput').value.trim() != '') {
+                                await formHandler('get', 'https://localhost:7181/api/Spots/filterSpots', 'searchForm')
+                                    .then((response) => {
+                                        if (typeof response.status != null && response.status === 400) {
+                                            Swal.fire({
+                                                icon: 'error',
+                                                title: 'Oops...',
+                                                text: 'Algo deu errado!',
+                                            })
+                                        } else {
+                                            setSpots(response.spots);
+                                            for (let i = 1; i <= response.spots[0].qtdPaginas; i++) {
+                                                setPageNumbers(i); 
+                                            }
+                                        }
+                                    });
+                            } else {
+                                Swal.fire({
+                                    icon: 'warning',
+                                    title: 'Atenção',
+                                    text: 'Digite no campo Pesquisa o que deseja encontrar!',
+                                })
+                            }
                         }}>
                             <i className="bi bi-search" />&nbsp;
                             Buscar
@@ -94,7 +104,7 @@ export const BodyForm = () => {
                                     <Card.Text>
                                         {spot.spotdescription}
                                     </Card.Text>
-                                    <Button variant="secondary" onClick={() => { handleOpenModalViewSpot(spot.spotid) }}>Ver detalhes</Button>
+                                    <Button variant="secondary" size='sm' onClick={() => { handleOpenModalViewSpot(spot.spotid) }}>Ver detalhes</Button>
                                 </Card.Body>
                             </Card>
                             <div><br></br></div>
@@ -102,7 +112,7 @@ export const BodyForm = () => {
                     ))
                 }
             </div>
-            <ModalViewSpot show={showModalViewSpot} onClose={handleCloseModalViewSpot} spotid={selectedSpotId}/>
+            <ModalViewSpot show={showModalViewSpot} onClose={handleCloseModalViewSpot} spotid={selectedSpotId} />            
         </Container>
     );
 }
